97f1366c19f6442b6f71f52f142c365fc27f7fc3
Colin Barber <colin.barber@ericsson.com>
2018-10-22 23:10:20 -0400

    trying to add server again

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import request
+from Server.TAGit._init_ import app, db
+from Server.TAGit.models.games import Game
+from Server.TAGit.models.user import User
+from flask import jsonify
+
+
+#tested
+@app.route('/', methods=['GET'])
+def default():
+    return "helloworld"
+
+#this method is being called in and registers a user to the database.
+@app.route('/createclient/', methods=['POST'])
+def addUserToDB():
+    content = request.get_json()
+    username = content['Username']
+    password = content['password']
+    email = content['email']
+
+    myuser = User(username=username,
+                  admin=0,
+                  password=password,
+                  email=email)
+    db.session.add(myuser)
+    db.session.commit()
+    return "user created"
+
+#I dont know why I wrote this
+@app.route('/deleteTable', methods=['DELETE'])
+def deleteTable():
+    Game.__table__.drop(db.engine)
+    return "DONE"
+
+#tested
+@app.route('/find_game/<int:post_id>', methods=['GET'])
+def searchUserById(post_id):
+    g = Game.query.filter_by(id=post_id).first()
+    json = jsonify(
+                    {
+                        "name": g.name,
+                        "questions": [g.question0, g.question1]
+                    }
+                   )
+    return json
+
+@app.route('/add_game/<int:post_id>', methods=['POST'])
+def searchUserById(post_id):
+    content = request.get_json()
+    name = content['name']
+    question0 = content['question0']
+    question1 = content['question1']
+
+    mygame = Game(name=name,
+                  question0=question0,
+                  question1=question1)
+    db.session.add(mygame)
+    db.session.commit()
+    return "game created"
+
+
+if __name__ == '__main__':
+    app.run()
+
