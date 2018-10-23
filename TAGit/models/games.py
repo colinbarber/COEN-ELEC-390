5dfb21ff959bbbc6e9e75f4e85505f75bc215ab0
Colin Barber <colin.barber@ericsson.com>
2018-10-22 23:01:41 -0400

    updated server branch

---
+++ TAGit/models/games.py
@@ -0,0 +1,18 @@
+from Server.TAGit._init_ import db
+
+# this is a python reprisentation of the user table in the database
+class Game(db.Model):
+    __tablename__ = 'Game'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = question0 = db.Column(db.String(64), unique=False)
+    question0 = db.Column(db.String(512), unique=False)
+    question1 = db.Column(db.String(512), unique=False)
+
+    def __init__(self, name, question0, question1):
+        self.name = name
+        self.question0 = question0
+        self.question1 = question1
+
+    def __repr__(self):
+        return '<Game %r>' % (self.name)
