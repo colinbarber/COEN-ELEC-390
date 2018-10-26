2353022f6cddd8f1d84d1fcea834d0830ae878c6
Constantina Roumeliotis <dina2395@hotmail.com>
2018-10-25 21:44:24 -0400

    Main page & attempt to get hints

--- app/src/main/java/com/example/roumeliotis/tagit/MainActivity.java
+++ app/src/main/java/com/example/roumeliotis/tagit/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.roumeliotis.tagit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue queue;
+    protected Button joinGameButton;
+    private String getFirstHintURL = "https://coen390-a-team.herokuapp.com/getfirsthint/";
+    private int gameId = 1;
+    private TextView hint;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hint = findViewById(R.id.hint);
+
+        url = getFirstHintURL + gameId;
+        System.out.print(url);
+        queue = Volley.newRequestQueue(this);
+        joinGameButton = findViewById(R.id.joinGameButton);
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                        url,
+                        null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            hint.setText(response.get("hint").toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                               System.out.println(error.toString());
+                        }
+                });
+
+                queue.add(jsonObjectRequest);
+            }
+        });
     }
 }
