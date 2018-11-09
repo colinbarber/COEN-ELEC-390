06e4c5923602be4d5f5615518d60bb32e0cf72cd
JN <jn@chengtarantino.com>
2018-11-09 12:11:25 -0500

    created volley services and added call to get game by name

--- app/src/main/java/com/example/roumeliotis/tagit/ServerHelper.java
+++ app/src/main/java/com/example/roumeliotis/tagit/ServerHelper.java
@@ -10,47 +10,62 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ServerHelper {
 
-    private static ServerHelper INSTANCE = null;
-    private static Context mContext;
-
     public static final String TAG = "ServerHelper";
-    private static final String base_url = "https://coen390-a-team.herokuapp.com";
-    RequestQueue mQueue;
+    private static final String base_url = "https://coen390-a-team.herokuapp.com/";
 
-    private ServerHelper(Context context){
-        mContext = context;
-        mQueue = getRequestQueue();
-    }
+    public void fetchGame(final String gameName, final Context context, final VolleyCallback callback){
+        /*
+        Fetches game from server and adds relevant components to SQLite db
+        callback can be used for further manipulation of response
+         */
 
-    public RequestQueue getRequestQueue(){
-        if (mQueue == null)
-            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
-        return mQueue;
-    }
+        Log.d(TAG, "fetchGame");
 
-    public static synchronized ServerHelper getInstance(Context context){
-        if (INSTANCE == null)
-            INSTANCE = new ServerHelper(context);
-        return INSTANCE;
-    }
-
-    public Game getGame(String gameName){
-        Log.d(TAG, "getGame");
-        Game game = null;
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,base_url+gameName,null,new Response.Listener<JSONObject>(){
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,base_url+"game/"+gameName,null, new Response.Listener<JSONObject>(){
             @Override
             public void onResponse(JSONObject response){
-                game = new Game(-1,response.getLong("game_id"),);
+                try {
+                    Game game = new Game(-1, response.getLong("game_id"), "Username", gameName,0 ); //TODO ADD USERNAME CALL AND ENDTIME CALLS WHEN SERVER RESPONSE IS CHANGED
+                    JSONArray jsonTeamIds = response.getJSONArray("team_ids");
+                    JSONArray jsonTeamNames = response.getJSONArray("team_names");
+                    JSONArray jsonTeamColours = response.getJSONArray("team_colours");
+                    JSONArray jsonHints = response.getJSONArray("hints");
+                    JSONArray jsonTagIds = response.getJSONArray("tag_ids");
+
+                    GameManager manager = new GameManager(context);
+                    long game_id = manager.insertGame(game);
+
+                    for(int i = 0; i < jsonTeamIds.length(); i++){
+                        manager.insertTeam(new Team(-1, game_id, jsonTagIds.getLong(i),jsonTeamNames.getString(i),jsonTeamColours.getString(i)));
+                    }
+
+                    for (int i = 0; i < jsonTagIds.length(); i++){
+                        manager.insertTag(new NFCTag(-1, jsonTagIds.getLong(i), game_id, jsonHints.getString(i)));
+                    }
+
+                    callback.onSuccess(response);
+                }
+                catch(JSONException e) {
+                    e.printStackTrace();
+                }
             }
         },new Response.ErrorListener(){
             @Override
-            public  void onErrorResponse(VolleyError error){
+            public void onErrorResponse(VolleyError error){
                 Log.d(TAG,error.toString());
+                callback.onError(error);
             }
         } );
+
+        Log.d(TAG, "request: " + request.toString());
+        VolleySingleton.getInstance(context).addToQueue(request);
+
+
     }
 }
