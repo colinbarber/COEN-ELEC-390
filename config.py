a590f56d6f5cecf17104e7eff54f86df5fdac697
William Bergamin <berw@videotron.ca>
2019-10-10 11:57:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 HEROKU_DATABASE_URI = "DATABASE_URI"
 LOCAL_DATABASE_URI = "postgres://wrybyfalquupcz:b70e82c803ecd592bdaef8ceb64f3e3025a50085057f64f9eeecdb8d53b9cf66@ec2-54-83-49-109.compute-1.amazonaws.com:5432/deqfrqbllfvbft"
-secret_key = '896mdxwxf59tp5xmhpz7w6j2z6oth9ex'
+secret_key = ''
